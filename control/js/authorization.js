d0921d29358ccf9cfd3b100a0e9db87adf8f5d2e
ab20153 <ab16240@edu.lu.lv>
2024-01-16 23:27:37 +0200

    commented the JS code

--- control/js/authorization.js
+++ control/js/authorization.js
@@ -1,11 +1,13 @@
+//class to make sure that user has been authorized to see the homepage
 class Authorization {
 	constructor() {
         document.querySelector("body").style.display = "none";
 		const auth = localStorage.getItem("auth");
-		this.validateAuth(auth);
+		this.validateAuthorization(auth);
 	}
 
-	validateAuth(auth) {
+	//method to redirect user back to login page if they're not authorized
+	validateAuthorization(auth) {
 		if (auth) {
             document.querySelector("body").style.display = "block";
 		} else {
@@ -13,14 +15,16 @@ class Authorization {
 		}
 	}
 
+	//method to redirect user back to login page and remove the auth local storage item
+	//when Log Out button is pressed
 	logOut() {
 		localStorage.removeItem("auth");
 		window.location.replace("index.html");
 	}
 }
 
+//check user's authorization and create an event listener for the Log Out button
 const auth = new Authorization();
-
 document.querySelector(".logout").addEventListener("click", (e) => {
 	auth.logOut();
 });
--- control/js/login.js
+++ control/js/login.js
@@ -1,3 +1,4 @@
+//login class to validate the login form fields
 class Login {
 	constructor(form, fields) {
 		this.form = form;
@@ -5,25 +6,29 @@ class Login {
 		this.validate();
 	}
 
+	//method to set a local storage item if username and password have been provided
 	validate() {
 		this.form.addEventListener("submit", (e) => {
 			e.preventDefault();
 			var error = false;
+			//validate each field (username, password)
 			this.fields.forEach((field) => {
 				const input = document.querySelector(`#${field}`);
 				if (this.validateFields(input) == false) {
 					error = true;
 				}
 			});
+			//if both fields validated, submit form and generate the local storage item
 			if (!error) {
-				//do login api here
 				localStorage.setItem("auth", true);
 				this.form.submit();
 			}
 		});
 	}
 
+	//method to validate a field
 	validateFields(field) {
+		//if field is empty, create an error message under the field
 		if (field.value.trim() === "") {
 			this.setStatus(
 				field,
@@ -37,6 +42,7 @@ class Login {
 	    }
     }
 
+	//method to generate or remove error messages under invalid fields
 	setStatus(field, message, success) {
 		const errorMessage = field.parentElement.querySelector(".errorMessage");
 
@@ -52,6 +58,7 @@ class Login {
 	}
 }
 
+//grab the form from the login page and run validation on its fields
 const form = document.querySelector(".loginForm");
 if (form) {
 	const fields = ["username", "password"];
